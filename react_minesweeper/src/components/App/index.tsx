863d818f8f4f3315988ed2155fa73ce376e62d4f
erik hirsch <ehirsch760@gmail.com>
2021-01-30 17:15:03 -0800

    header styling

---
+++ react_minesweeper/src/components/App/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import NumberDisplay from "../NumberDispaly";
+import "./App.scss";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="Header">
+        <NumberDisplay value={0} />
+        <div className="Face">
+          <span role="img" aria-label="face">
+            🧙🏽‍♂️
+          </span>
+        </div>
+        <NumberDisplay value={23} />
+      </div>
+      <div className="Body">Body</div>
+    </div>
+  );
+};
+
+export default App;
